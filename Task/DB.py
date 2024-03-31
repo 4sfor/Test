9ed63dc939ff6315d626586648d1e91915dc8fa2
Aleksandr Safonov <siaalex03@gmail.com>
2024-03-31 20:52:12 +0400

    changed SQL and logic

--- Task/DB.py
+++ Task/DB.py
@@ -1,38 +1,62 @@
+import sys
+
 import psycopg
 
 
 #Подключаемся к postgresql
-def connect_to_db():
-    try:
-        conn = psycopg.connect(dbname="TestTask", user="postgres", password="qwer1833", host="localhost", port="5432")
-        print('Connected  OK')
-        return conn
-    except psycopg.DatabaseError as er:
-        print('Connection failed', er)
-
-
-#получаем из базы информацию по заказам
-def take_order(args):
-    conn = connect_to_db()
+try:
+    conn = psycopg.connect(dbname="TestTask", user="postgres", password="qwer1833", host="localhost", port="5432")
     cur = conn.cursor()
-    args_unpacking = ", ".join(args)
-    resultSQL = cur.execute(
+    print('Connected  OK')
+
+except psycopg.DatabaseError as er:
+    print('Connection failed', er)
+
+
+#получаем из базы информацию по заказам , функция генератор
+def take_order(arg):
+
+    sql_from_order_product = cur.execute(
         f'''
-        SELECT order_product.order_id, order_product.product_id , order_product.amount, product_shelf.main_shelf,  product.name, shelf.name FROM 
-        order_product INNER JOIN product_shelf ON order_product.product_id = product_shelf.product_id INNER JOIN 
-        product ON order_product.product_id = product.id INNER JOIN shelf ON product_shelf.shelf_id=shelf.id WHERE order_product.order_id IN ({args_unpacking}) ORDER BY shelf.name
+        SELECT order_id, product_id, amount From order_product WHERE order_id IN ({arg})
         '''
     )
-    result = [list(x)for x in resultSQL.fetchall()]
-    for element in result:
-        if element[3]:
-            for elem in result:
-                if element[0:2] == elem[0:2] and elem[3] != element[3]:
-                    element.append(elem[5])
-                    result.remove(elem)
-        else:
-            continue
+    result_from_order_product = sql_from_order_product.fetchall()
 
-    return result
+    for element in result_from_order_product:
+
+        sql_from_product_shelf = cur.execute(
+            f'''
+                    SELECT  main_shelf,shelf_id FROM product_shelf WHERE product_id={element[1]} 
+                    '''
+        )
+        result_from_product_shelf = sql_from_product_shelf.fetchall()
+
+        sql_from_product = cur.execute(
+            f'''
+                   SELECT name FROM product WHERE id={element[1]}
+                   '''
+        )
+        result_from_product = sql_from_product.fetchall()
+
+        for_sql__from_shelf=', '.join([str(x[1]) for x in result_from_product_shelf])
+        sql_from_shelf = cur.execute(
+            f'''
+                    SELECT id, name FROM shelf WHERE id IN ({for_sql__from_shelf})
+                    '''
+        )
+        result_from_shelf = [list(x) for x in sql_from_shelf.fetchall()]
+
+        for shelf in result_from_shelf:
+            for pr_shelf in result_from_product_shelf:
+                if shelf[0] == pr_shelf[1]:
+                    shelf.append(pr_shelf[0])
+        result_from_shelf.sort(key=lambda x: x[2], reverse=True)
+
+        result = [element, result_from_product, *result_from_shelf]
+
+        yield result
+
+
 
 
--- Task/basic.py
+++ Task/basic.py
@@ -9,28 +9,33 @@ def take_number_orders():
 
 #Получение товаров на стеллажах
 def take_shelf():
-    data_from_db = take_order(take_number_orders())
-
     shelf = {}
-    for element in data_from_db:
-        shelf[element[5]] = [elem for elem in data_from_db if element[5] == elem[5]]
+    for number in take_number_orders():
+        for element in take_order(number):
+            key = element[2][1]
+            if key not in shelf:
+                shelf[key] = []
+            shelf[key].append(element)
     sorted_shelf = dict(sorted(shelf.items()))
-
     return sorted_shelf
 
 
 #Вывод в консоль
 def output():
     dict_result = take_shelf()
-    for element in dict_result:
-        print(f'===Стеллаж {element}')
-        for element_shelf in dict_result[element]:
-            if len(element_shelf) > 6:
-                print(f'{element_shelf[4]} (id={element_shelf[1]})\nзаказ {element_shelf[0]}, {element_shelf[2]} шт\n'
-                      f'доп стеллаж: {",".join(element_shelf[6:])}\n')
+    for key in dict_result:
+        print(f'===Стеллаж {key}')
+        for element_shelf in dict_result[key]:
+
+            if len(element_shelf) > 3:
+                print(f'{element_shelf[1][0][0]} (id={element_shelf[0][1]})\nзаказ {element_shelf[0][0]}, {element_shelf[0][2]} шт\n'
+                      f'доп стеллаж: {",".join([x[1] for x in element_shelf[3:]])}\n')
             else:
-                print(f'{element_shelf[4]} (id={element_shelf[1]})\nзаказ {element_shelf[0]}, {element_shelf[2]} шт\n')
+                print(f'{element_shelf[1][0][0]} (id={element_shelf[0][1]})\nзаказ {element_shelf[0][0]}, {element_shelf[0][2]} шт\n')
+
+
 
 
 
 output()
+
